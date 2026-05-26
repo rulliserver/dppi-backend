7454d4746d4671362564ad1e966d891241e233e8
Agus Rulli Sofian <90967203+rulliserver@users.noreply.github.com>
2026-05-26 11:19:17 +0700

    fix data pasnas

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     env_logger::Builder::from_env(
-        env_logger::Env::default().default_filter_or("info,actix_web=info, debug"),
+        env_logger::Env::default().default_filter_or("info,actix_web=info"),
     )
     .format(|buf, record| {
         use std::io::Write;
